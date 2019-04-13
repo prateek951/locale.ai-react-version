65e50d30f49aa94c2f28b7e2ea7f4d538713838f
prateek951 <prince.cenation@gmail.com>
2019-04-13 21:27:07 +0530

    'uiuo'

--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -6,21 +6,24 @@ import { toast } from "react-toastify";
  * @interceptors for response in case of error
  */
 
-axios.interceptors.response.use(null, error => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-  //All unexpected errors must be logged out to the user,
-  // using the logger middleware
+if(process.env.NODE_ENV === "development") { 
+  axios.interceptors.response.use(null, error => {
+    const expectedError =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+    //All unexpected errors must be logged out to the user,
+    // using the logger middleware
+  
+    if (!expectedError) {
+      winston.log(0, `Logging error ${error}`);
+      //For displaying the toast notifications we can use the toast object
+      toast.error("An unexpected error occured");
+    }
+    return Promise.reject(error);
+  });
+}
 
-  if (!expectedError) {
-    winston.log(0, `Logging error ${error}`);
-    //For displaying the toast notifications we can use the toast object
-    toast.error("An unexpected error occured");
-  }
-  return Promise.reject(error);
-});
 
 export default {
   get: axios.get,
